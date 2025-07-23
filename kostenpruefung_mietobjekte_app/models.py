7bd87897a187e07a7e0551288886d24c5ac4d6b3
JonnyDKahle <jonnykahle@yahoo.de>
2025-07-23 18:54:22 +0200

    Add Mietobjekt model

--- kostenpruefung_mietobjekte_app/models.py
+++ kostenpruefung_mietobjekte_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Mietobjekt(models.Model):
+    name = models.CharField(max_length=255)
+    strasse_hausnummer = models.CharField(max_length=255)
+    plz = models.IntegerField()
+    ort = models.CharField(max_length=100)
+    land = models.CharField(max_length=100)
+    kaufdatum = models.DateField()
+    kaufpreis = models.FloatField()
+    darlehen = models.FloatField()
+    grundschuld = models.FloatField()
+    FARBE_CHOICES = [
+        ('rot', 'Rot'),
+        ('blau', 'Blau'),
+        ('gruen', 'Grün'),
+        ('gelb', 'Gelb'),
+        ('schwarz', 'Schwarz'),
+    ]
+    farbe = models.CharField(max_length=20, choices=FARBE_CHOICES)
+
+    def __str__(self):
+        return self.name
