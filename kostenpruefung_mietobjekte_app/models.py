1f708d201913ddebb546e94cba40014789035cb9
JonnyDKahle <jonnykahle@yahoo.de>
2025-07-24 13:54:18 +0200

    add Rechnung, Rechnungsart, and Lieferant models

--- kostenpruefung_mietobjekte_app/models.py
+++ kostenpruefung_mietobjekte_app/models.py
@@ -29,7 +29,7 @@ class Mieter(models.Model):
     vorname = models.CharField(max_length=255)
     nachname = models.CharField(max_length=255)
     strasse_hausnummer = models.CharField(max_length=255)
-    plz = models.IntegerField()
+    plz = models.CharField()
     ort = models.CharField(max_length=100)
     land = models.CharField(max_length=100)
     telefon = models.CharField(max_length=100)
@@ -43,7 +43,7 @@ class Mieter(models.Model):
     nebenkosten = models.DecimalField()
     kaution = models.DecimalField()
 
-    mietobjekte = models.ManyToManyField(Mietobjekt)
+    mietobjekte = models.ManyToManyField('Mietobjekt')
 
     @property
     def mietstatus(self):
@@ -57,3 +57,47 @@ class Mieter(models.Model):
         
     def __str__(self):
         return f"{self.vorname} {self.nachname}"
+    
+
+class Rechnung(models.Model):
+    datum = models.DateField()
+    rechnungsnummer = models.CharField(max_length=255)
+    name = models.CharField(max_length=255) # Descriptive name to identify the payment
+    betrag = models.DecimalField()
+    art = models.ForeignKey('Rechnungsart', on_delete=models.PROTECT)
+    lieferant = models.ManyToManyField('Lieferant')
+    bezahlt_am = models.DateField()
+    #offener_betrag = ... # What is the idea here? Should there be a place where costs that where the due date is now are listed? Where do they come from?
+    #konto = ... # Add model first
+    mietobjekte = models.ManyToManyField('Mietobjekt') # What is the idea of the Percentage? Is the idea all the apartments of a tenant are listed? Confusing?
+    #Anteil = ... # This is somehow conneted to the column above. Only if the percentage of the above column sums up to 100, the "Rechnung" can be created.
+
+    def __str__(self):
+        return f"Rechnung {self.rechnungsnummer} - {self.name}"
+    
+class Rechnungsart(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+class Lieferant(models.Model):
+    firmenname = models.CharField(max_length=255)
+    vorname = models.CharField(max_length=255)
+    nachname = models.CharField(max_length=255)
+    strasse = models.CharField(max_length=255)
+    hausnummer = models.CharField(max_length=10)
+    plz = models.CharField(max_length=10) # 2 - 10 characters (numbers and letters)
+    ort = models.CharField(max_length=100)
+    land = models.CharField(max_length=100)
+    e_mail = models.EmailField()
+    telefon = models.CharField(max_length=100)
+
+# class Konto(models.Model):
+#     buchungstag = models.DateField()
+#     kontoinhaber = models.CharField(max_length=255) # 
+#     buchungstext =      # (Aus Kontoauszug)
+#     wertstellung =      # (Datum)
+#     buchungsart =       # (Lastschrift - z.B. Ueberweisung)
+#     mieter = models.ManyToManyField('Mieter')
+#     lieferanten = models.ManyToManyField('Lieferant')
