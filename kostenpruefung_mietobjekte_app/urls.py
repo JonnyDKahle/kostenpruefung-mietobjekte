7c779fdfd45afd147c68ad748523f47046f2203f
JonnyDKahle <jonnykahle@yahoo.de>
2025-07-24 20:32:18 +0200

    Added views: rechnungen, kostenarten, lieferanten

--- kostenpruefung_mietobjekte_app/urls.py
+++ kostenpruefung_mietobjekte_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import test_view, objekt_index, mieter
+from .views import test_view, objekt_index, mieter, rechnungen, kostenarten, lieferanten
 
 
 
@@ -7,4 +7,7 @@ urlpatterns = [
     path('test/', test_view, name='test'),
     path('', objekt_index, name='objekt_index'),
     path('mieter/', mieter, name='mieter'),
+    path('rechnungen/', rechnungen, name='rechnungen'),
+    path('kostenarten/', kostenarten, name='kostenarten'),
+    path('lieferanten/', lieferanten, name='lieferanten'),
 ]
--- kostenpruefung_mietobjekte_app/views.py
+++ kostenpruefung_mietobjekte_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Mietobjekt, Mieter
+from .models import Mietobjekt, Mieter, Rechnung, Rechnungsart, Lieferant
 
 # Create your views here.
 def test_view(request):
@@ -13,3 +13,15 @@ def objekt_index(request):
 def mieter(request):
     mieter = Mieter.objects.all()
     return render(request, 'kostenpruefung_mietobjekte_app/mieter.html', {'mieter': mieter})
+
+def rechnungen(request):
+    rechnungen = Rechnung.objects.all()
+    return render(request, 'kostenpruefung_mietobjekte_app/rechnungen.html', {'rechnungen': rechnungen})
+
+def kostenarten(request):
+    kostenarten = Rechnungsart.objects.all()
+    return render(request, 'kostenpruefung_mietobjekte_app/kostenarten.html', {'kostenarten': kostenarten})
+
+def lieferanten(request):
+    lieferanten = Lieferant.objects.all()
+    return render(request, 'kostenpruefung_mietobjekte_app/lieferant.html', {'lieferanten': lieferanten})
