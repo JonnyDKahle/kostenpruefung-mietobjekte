71e78dff7b58ec98651fbf4d752c71e4c1e6402f
JonnyDKahle <jonnykahle@yahoo.de>
2025-07-24 18:41:04 +0200

    Added view "Objekte"

--- kostenpruefung_mietobjekte_app/urls.py
+++ kostenpruefung_mietobjekte_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import test_view
+from .views import test_view, objekt_index
 
 
 
 urlpatterns = [
-    path('test/', test_view, name='test/'),
+    path('test/', test_view, name='test'),
+    path('', objekt_index, name='objekt_index'),
 ]
--- kostenpruefung_mietobjekte_app/views.py
+++ kostenpruefung_mietobjekte_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Mietobjekt
 
 # Create your views here.
 def test_view(request):
     return HttpResponse("it works")
+
+def objekt_index(request):
+    objekte = Mietobjekt.objects.all()
+    return render(request, 'kostenpruefung_mietobjekte_app/objekt_index.html', {'objekte': objekte})
