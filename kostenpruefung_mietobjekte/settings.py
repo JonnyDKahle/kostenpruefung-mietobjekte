95704a83bbdabd11e53fb7a0ddda9d82845ab732
JonnyDKahle <jonnykahle@yahoo.de>
2025-08-13 22:21:48 +0200

    Dates are displayed in german format

--- kostenpruefung_mietobjekte/settings.py
+++ kostenpruefung_mietobjekte/settings.py
@@ -105,13 +105,20 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de-de'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
+
+# German date formats
+DATE_FORMAT = 'd.m.Y'
+DATETIME_FORMAT = 'd.m.Y H:i'
+SHORT_DATE_FORMAT = 'd.m.Y'
 
 
 # Static files (CSS, JavaScript, Images)
